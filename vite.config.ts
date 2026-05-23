359428b8272e0810c97ab7ef0133a3bb17830624
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2026-05-23 10:40:48 +0530

    added tailwind and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path";
+import { defineConfig } from "vite";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
